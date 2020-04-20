ea3fa57941aeebc320af713ece74b9a38c0e32a4
Noah Huetter <noahhuetter@gmail.com>
2020-04-20 10:41:40 +0200

    Add plotting for different MFCC implementations

--- audio/speechrecognizer.py
+++ audio/speechrecognizer.py
@@ -2,7 +2,7 @@
 # @Author: Noah Huetter
 # @Date:   2020-04-16 16:59:06
 # @Last Modified by:   Noah Huetter
-# @Last Modified time: 2020-04-20 09:20:39
+# @Last Modified time: 2020-04-20 10:40:20
 
 import audioutils as au
 import mfcc_utils as mfu
@@ -149,7 +149,7 @@ def load_data2(max_len=11):
     print('calculate mfcc with librosa')
     x_train_mfcc = []
     for waveCtr in tqdm(range(x_train.shape[0])):
-      mfcc = librosa.feature.mfcc(x_train[waveCtr], sr=16000)
+      mfcc = librosa.feature.mfcc(x_train[waveCtr], sr=16000, n_mfcc=13, hop_length=1024)
       if (max_len > mfcc.shape[1]):
         pad_width = max_len - mfcc.shape[1]
         mfcc = np.pad(mfcc, pad_width=((0, 0), (0, pad_width)), mode='constant')
@@ -158,7 +158,7 @@ def load_data2(max_len=11):
       x_train_mfcc.append(mfcc)
     x_test_mfcc = []
     for waveCtr in tqdm(range(x_test.shape[0])):
-      mfcc = librosa.feature.mfcc(x_test[waveCtr], sr=16000)
+      mfcc = librosa.feature.mfcc(x_test[waveCtr], sr=16000, n_mfcc=13, hop_length=1024)
       if (max_len > mfcc.shape[1]):
         pad_width = max_len - mfcc.shape[1]
         mfcc = np.pad(mfcc, pad_width=((0, 0), (0, pad_width)), mode='constant')
@@ -246,37 +246,85 @@ def load_data3(max_len=11):
 
 ##################################################
 # plottery
-def plotInputDifference(mfcc_a, mfcc_b):
+def plotInputDifference(mfccs, names):
   """
     Plot different mfcc inputs
   """
   import matplotlib.pyplot as plt
+  from matplotlib import colors
+  import matplotlib as mpl
+  
+  sampleno_left = 5
+  sampleno_right = 6
+
   plt.style.use('seaborn-bright')
   # t = np.linspace(0, nSamples/fs, num=nSamples)
   # f = np.linspace(0.0, fs/2.0, fft_len/2)
-  fig, axs = plt.subplots(2, 2)
+  fig, axs = plt.subplots(4, 2)
+  fig.set_size_inches(8,8)
+  
+  # cmap = mpl.cm.cool
+  # norm = mpl.colors.Normalize(vmin=0, vmax=100)
+
+  cm = 'cool'#mpl.cm.ScalarMappable(norm=norm, cmap=cmap)
+  # fig.colorbar(cm,
+  #              cax=ax, orientation='horizontal', label='Some Units')
+  # norm = colors.Normalize(vmin=vmin, vmax=vmax)
+  # for im in images:
+  #   im.set_norm(norm)
+
+  x_train, y_train, x_test, y_test = au.load_snips_data(trainsize = trainsize, testsize = testsize)
   ax=axs[0,0]
-  ax.plot(mfcc_a[0].reshape((-1,)), label='a')
-  ax.plot(mfcc_b[0].reshape((-1,)), label='b')
-  ax.set_xlabel('mel bin')
-  ax.set_ylabel('mel coefficient')
-  # ax.set_xlim(0,nSamples/fs)
-  # ax.set_ylim(-25000,25000)
+  ax.plot(x_test[sampleno_left])
+  ax.set_title('raw hotword')
   ax.grid(True)
-  ax.legend()
+  ax=axs[0,1]
+  ax.plot(x_test[sampleno_right])
+  ax.set_title('raw coldword')
+  ax.grid(True)
 
+  axctr = 0
+  for mfcc in mfccs:
+    ax=axs[axctr+1,0]
+    cm = ax.pcolor(np.transpose(mfcc[sampleno_left].reshape((mfcc[sampleno_left].shape[0], mfcc[sampleno_left].shape[1]))),cmap='RdBu')
+    ax.set_xlabel('frame')
+    ax.set_ylabel('MFCC')
+    ax.set_xlim(0,62)
+    ax.set_ylim(0,13)
+    ax.set_title(names[axctr]+' - hotword')
+    ax.grid(True)
+    fig.colorbar(cm, ax=ax)
+
+    ax=axs[axctr+1,1]
+    cm = ax.pcolor(np.transpose(mfcc[sampleno_right].reshape((mfcc[sampleno_right].shape[0], mfcc[sampleno_right].shape[1]))),cmap='RdBu')
+    ax.set_xlabel('frame')
+    ax.set_ylabel('MFCC')
+    ax.set_xlim(0,62)
+    ax.set_ylim(0,13)
+    ax.set_title(names[axctr]+' - coldword')
+    ax.grid(True)
+    fig.colorbar(cm, ax=ax)
+
+    axctr = axctr + 1
+
+  fig.tight_layout()
+  fig.savefig('test2png.png', dpi=100)
   plt.show()
-
 
 
 ##################################################
 # MAIN
 ##################################################
 
-# x_train_mfcc, _, _, _ = load_data()
-# x_train_mfcc3, _, _, _ = load_data3()
-# plotInputDifference(x_train_mfcc, x_train_mfcc3)
-# exit()
+x_train_mfcc, x_test_mfcc, y_train, y_test = load_data()
+x_train_mfcc2, x_test_mfcc2, y_train2, y_test2 = load_data2()
+x_train_mfcc3, x_test_mfcc3, y_train3, y_test3 = load_data3()
+x_test_mfcc2 = x_test_mfcc2.transpose((0,2,1,3))
+print(x_test_mfcc.shape)
+print(x_test_mfcc2.shape)
+print(x_test_mfcc3.shape)
+plotInputDifference([x_test_mfcc, x_test_mfcc2, x_test_mfcc3], ['custom implemetation', 'librosa', 'tensorflow'])
+exit()
 
 
 x_train_mfcc, x_test_mfcc, y_train, y_test = load_data()
@@ -292,7 +340,7 @@ print(y_test)
 # Build model
 model = get_model(inp_shape=x_train_mfcc.shape[1:], num_classes = 1)
 model.summary()
-train_set, train_labels, test_set, test_labels = train(model, batchSize = 10, epochs = 500)
+train_set, train_labels, test_set, test_labels = train(model, batchSize = 10, epochs = 100)
 
 model.summary()
 print(x_test_mfcc.shape)
