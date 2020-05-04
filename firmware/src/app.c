ee710a639d556e9c91ccace6e0571c61958661d7
Noah Huetter <noahhuetter@gmail.com>
2020-05-04 14:40:07 +0200

    move other test to static data

--- firmware/src/app.c
+++ firmware/src/app.c
@@ -2,7 +2,7 @@
 * @Author: Noah Huetter
 * @Date:   2020-04-15 11:16:05
 * @Last Modified by:   Noah Huetter
-* @Last Modified time: 2020-05-04 14:24:53
+* @Last Modified time: 2020-05-04 14:38:18
 */
 #include "app.h"
 #include <stdlib.h>
@@ -39,6 +39,13 @@
 /*------------------------------------------------------------------------------
  * Private data
  * ---------------------------------------------------------------------------*/
+
+
+static float netInput[AI_NET_INSIZE_BYTES/4];
+static float netOutput[AI_NET_OUTSIZE_BYTES/4];
+static FASTRAM_BSS int16_t inFrameBuf[1024*16];
+
+
 /*------------------------------------------------------------------------------
  * Prototypes
  * ---------------------------------------------------------------------------*/
@@ -57,8 +64,8 @@ int8_t appHifMfccAndInference(uint8_t *args)
   (void)args;
   int ret;
   
-  int16_t *in_data=NULL, *out_mfccs;
-  float *out_data=NULL, *mfccs=NULL, tmpf;
+  int16_t *out_mfccs;
+  float tmpf;
   
   uint8_t tag;
   uint16_t in_x, in_y;
@@ -71,28 +78,19 @@ int8_t appHifMfccAndInference(uint8_t *args)
 
   prfEvent("aiGetInputShape");
 
-  // printf("Got input shape (%d, %d)\n", in_x, in_y);
-
-  // 1. Receive in_y frames and process their MFCC
-  in_data = malloc(AUD_MFCC_FRAME_SIZE_BYES);
-  mfccs = malloc(AI_NET_INSIZE_BYTES);
-  out_data = malloc(AI_NET_OUTSIZE_BYTES);
-
-  prfEvent("malloc");
-
   for(int frameCtr = 0; frameCtr < in_y; frameCtr++)
   {
     // 2. Calculate MFCC
-    len = hiReceive(in_data, AUD_MFCC_FRAME_SIZE_BYES, DATA_FORMAT_S16, &tag);    
+    len = hiReceive(inFrameBuf, AUD_MFCC_FRAME_SIZE_BYES, DATA_FORMAT_S16, &tag);    
     printf("received %d \n", len);
 
-    audioCalcMFCCs(in_data, &out_mfccs);
+    audioCalcMFCCs(inFrameBuf, &out_mfccs);
 
     // copy to net in buffer and cast to float
     for(int mfccCtr = 0; mfccCtr < in_x; mfccCtr++)
     {
       tmpf = (float)out_mfccs[mfccCtr];
-      mfccs[frameCtr*in_x + mfccCtr] = tmpf;
+      netInput[frameCtr*in_x + mfccCtr] = tmpf;
     }
 
     // signal host that we are ready
@@ -102,22 +100,16 @@ int8_t appHifMfccAndInference(uint8_t *args)
   prfEvent("receive and MFCC");
 
   // 3. Run inference
-  ret = aiRunInference((void*)mfccs, (void*)out_data);
+  fprintf(&huart4, "inference..");
+  ret = aiRunInference((void*)netInput, (void*)netOutput);
+  fprintf(&huart4, "Prediction: %f\n", netOutput[0]);
   prfEvent("inference");
   hiSendMCUReady();
 
   // 4. report back mfccs and net out
-  hiSendF32(mfccs, AI_NET_INSIZE, 0x20);
-  hiSendF32(out_data, AI_NET_OUTSIZE, 0x21);
+  hiSendF32(netInput, AI_NET_INSIZE, 0x20);
+  hiSendF32(netOutput, AI_NET_OUTSIZE, 0x21);
   
-  prfEvent("send");
-
-  // cleanup
-  free(in_data);
-  free(mfccs);
-  free(out_data);
-
-  prfEvent("free");
   prfStop();
   return ret;
 }
@@ -140,17 +132,8 @@ int8_t appHifMicMfccInfere(uint8_t *args)
   float tmpf;
   int ret;
 
-  static float mfccs[AI_NET_INSIZE_BYTES/4];
-  static float out_data[AI_NET_OUTSIZE_BYTES/4];
-  static FASTRAM_BSS int16_t inFrameBuf[1024*16];
-
   // get net info
   aiGetInputShape(&in_x, &in_y);
-
-  // alocate memory
-  // mfccs = malloc(AI_NET_INSIZE_BYTES);
-  // out_data = malloc(AI_NET_OUTSIZE_BYTES);
-  // inFrameBuf = malloc(2*1024*16);
 
   // start continuous mic sampling
   micContinuousStart();
@@ -179,7 +162,7 @@ int8_t appHifMicMfccInfere(uint8_t *args)
     for(int mfccCtr = 0; mfccCtr < in_x; mfccCtr++)
     {
       tmpf = (float)out_mfccs[mfccCtr];
-      mfccs[frameCtr*in_x + mfccCtr] = tmpf;
+      netInput[frameCtr*in_x + mfccCtr] = tmpf;
     }
   } 
 
@@ -188,21 +171,16 @@ int8_t appHifMicMfccInfere(uint8_t *args)
 
   // 3. Run inference
   fprintf(&huart4, "inference..");
-  ret = aiRunInference((void*)mfccs, (void*)out_data);
+  ret = aiRunInference((void*)netInput, (void*)netOutput);
+  fprintf(&huart4, "Prediction: %f\n", netOutput[0]);
 
   // signal host that we are ready
   hiSendMCUReady();
 
   // 4. report back mfccs and net out
   hiSendS16(inFrameBuf, 1024*16, 0x30);
-  hiSendF32(mfccs, AI_NET_INSIZE, 0x31);
-  hiSendF32(out_data, AI_NET_OUTSIZE, 0x32);
-
-  fprintf(&huart4, "Prediction: %f\n", out_data[0]);
-
-  // free(inFrameBuf);
-  // free(mfccs);
-  // free(out_data);
+  hiSendF32(netInput, AI_NET_INSIZE, 0x31);
+  hiSendF32(netOutput, AI_NET_OUTSIZE, 0x32);
 
   return ret;
 }
