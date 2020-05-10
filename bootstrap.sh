ff654cfea4031ae474cc02bad636e9bb0eb1b29a
Noah Huetter <noahhuetter@gmail.com>
2020-05-10 12:05:40 +0200

    add bootstrap script to setup all

---
+++ bootstrap.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# init submodules
+git submodule update --init --recursive
+
+# create virtual env
+cd audio/
+virtualenv -p python3.7 venv
+
+# add paths to virtualenv
+echo "export PYTHONPATH=`pwd`:$PYTHONPATH" >> venv/bin/activate
+echo "export PYTHONPATH=`pwd`/implement/nnom/scripts:$PYTHONPATH" >> venv/bin/activate
+
+# install packages
+pip install -r requirements.txt
+
