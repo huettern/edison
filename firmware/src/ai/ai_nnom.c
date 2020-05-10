36d7f0a77a748295a7615a2956a7dcdf929e1e54
Noah Huetter <noahhuetter@gmail.com>
2020-05-10 15:42:56 +0200

    work on firmware

--- firmware/src/ai/ai_nnom.c
+++ firmware/src/ai/ai_nnom.c
@@ -1,14 +1,53 @@
+/*
+* @Author: Noah Huetter
+* @Date:   2020-04-15 11:16:05
+* @Last Modified by:   Noah Huetter
+* @Last Modified time: 2020-05-10 15:10:15
+*/
 
 #include "ai_nnom.h"
 
 #include "nnom.h"
 #include "kws_nnom/weights.h"
 
+/*------------------------------------------------------------------------------
+ * Data
+ * ---------------------------------------------------------------------------*/
+nnom_model_t *model;
+
+/*------------------------------------------------------------------------------
+ * Publics
+ * ---------------------------------------------------------------------------*/
+
+/**
+ * @brief Used to test
+ * @details [long description]
+ */
 void aiNnomTest(void)
 {
 #ifdef NNOM_VERIFICATION
-  nnom_model_t *model;
   model = nnom_model_create();
   model_run(model);
 #endif
+}
+
+/**
+ * @brief Init mode
+ * @details [long description]
+ *  
+ * @return 
+ */
+void aiNnomInit(void)
+{
+  model = nnom_model_create();
+}
+
+int aiNnomRunInference(void* in_data, void* out_data)
+{
+  // copy data to net input
+  memcpy(nnom_input_data, in_data, sizeof(nnom_input_data)*sizeof(int8_t));
+  // run prediciton
+  model_run(model);
+  // copy prediction back
+  memcpy(out_data, nnom_output_data, sizeof(nnom_output_data)*sizeof(int8_t));
 }
--- firmware/src/ai/ai_nnom.h
+++ firmware/src/ai/ai_nnom.h
@@ -2,3 +2,5 @@
 
 
 void aiNnomTest(void);
+void aiNnomInit(void);
+int aiNnomRunInference(void* in_data, void* out_data);
