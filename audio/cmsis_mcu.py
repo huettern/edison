f86dda4cf433323b9d1d31f175e3740f9bdc4ad4
Noah Huetter <noahhuetter@gmail.com>
2020-05-21 10:40:18 +0200

    simple receive script
    Signed-off-by: Noah Huetter <noahhuetter@gmail.com>

---
+++ audio/cmsis_mcu.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# @Author: Noah Huetter
+# @Date:   2020-05-20 18:07:22
+# @Last Modified by:   Noah Huetter
+# @Last Modified time: 2020-05-21 10:35:30
+
+
+import mcu_util as mcu
+
+
+data, tag = mcu.receiveData(timeout=50000)
+print('Received %s type with tag 0x%x len %d: %s' % (data.dtype, tag, data.shape[0], data))
+data, tag = mcu.receiveData(timeout=50000)
+print('Received %s type with tag 0x%x len %d: %s' % (data.dtype, tag, data.shape[0], data))
+data, tag = mcu.receiveData(timeout=50000)
+print('Received %s type with tag 0x%x len %d: %s' % (data.dtype, tag, data.shape[0], data))
+data, tag = mcu.receiveData(timeout=50000)
+print('Received %s type with tag 0x%x len %d: %s' % (data.dtype, tag, data.shape[0], data))
+data, tag = mcu.receiveData(timeout=50000)
+print('Received %s type with tag 0x%x len %d: %s' % (data.dtype, tag, data.shape[0], data))
+data, tag = mcu.receiveData(timeout=50000)
+print('Received %s type with tag 0x%x len %d: %s' % (data.dtype, tag, data.shape[0], data))
+data, tag = mcu.receiveData(timeout=50000)
+print('Received %s type with tag 0x%x len %d: %s' % (data.dtype, tag, data.shape[0], data))
+data, tag = mcu.receiveData(timeout=50000)
+print('Received %s type with tag 0x%x len %d: %s' % (data.dtype, tag, data.shape[0], data))
+
